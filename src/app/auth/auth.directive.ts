a4d70c685edb96bbdad4abf1762c9861dfe82c06
e.alzoubi <e.alzoubi@classera.com>
2025-02-14 18:25:44 +0300

    some code

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, effect, inject, input } from '@angular/core';
+import { Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 
@@ -10,15 +10,19 @@ export class AuthDirective {
 
   useType = input.required<Permission>({alias:'appAuth'});
   private authServes = inject(AuthService);
+  private templateRef = inject(TemplateRef);
+  private viewContineRef = inject(ViewContainerRef);
 
 
   constructor() { 
     effect(() => {
       if(this.authServes.activePermission() === this.useType()){
-        console.log("show the text");
+        // console.log("show the text");
+        this.viewContineRef.createEmbeddedView(this.templateRef);
       }
       else{
-        console.log("dont show the text");
+        // console.log("dont show the text");
+        this.viewContineRef.clear();
       }
     })
 
